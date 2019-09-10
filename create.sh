66dc65eb178a07a7d410a9b866d8695a375a9c20
Blas Medina <r.blas.m.c@gmail.com>
2019-09-10 20:16:22 -0300

    fix varios

--- create.sh
+++ create.sh
@@ -16,20 +16,18 @@ mkdir /data
 # Create App 000
 mkdir -p /data/app-000
 
+cd /data/app-000
 echo "const http = require('http');
-
 const HOSTNAME = '::';
 const PORT = 3000;
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('App-000');
 });
-
 server.listen(PORT, HOSTNAME, () => {
     console.log(`Server running at ${HOSTNAME} on port ${PORT}.`);
 });
-" >> /data/app-000/index.js
+">>index.js
 
 node /data/app-000/index.js
