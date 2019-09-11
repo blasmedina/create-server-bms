c229056c660359bd3eec5cac63ecffc24ead78d8
Blas Medina <r.blas.m.c@gmail.com>
2019-09-10 21:58:06 -0300

    fix varios

--- create-app-test.sh
+++ create-app-test.sh
@@ -16,3 +16,4 @@ server.listen(PORT, HOSTNAME, () => {
     console.log("Server run");
 });
 EOL
+cd $DIR_INIT
--- create.sh
+++ create.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DIR_INIT=$PWD
 DIRECTORY="/data"
 # apt-get update
 # source install-git.sh
