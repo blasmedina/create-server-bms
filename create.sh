e33ad77d86abedde4e7c1105662441c436fafcb0
Blas Medina <r.blas.m.c@gmail.com>
2019-09-10 21:27:43 -0300

    fix varios

--- create.sh
+++ create.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 DIRECTORY="/data"
-apt-get update
+# apt-get update
 # source install-git.sh
 # source install-nodejs.sh
 # source install-pm2.sh
-rm -rf $DIRECTORY
-source create-app-000.sh
+# rm -rf $DIRECTORY
+# source create-app-000.sh
 
 pm2 delete all
 
