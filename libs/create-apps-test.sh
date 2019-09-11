7c2b9091b73e159b6926e3dae1db8c70f37fc513
Blas Medina <r.blas.m.c@gmail.com>
2019-09-11 18:21:23 -0300

    fix varios

--- libs/create-apps-test.sh
+++ libs/create-apps-test.sh
@@ -8,7 +8,7 @@ for i in {0..9}
 do
     # APP_TEST_NAME="app-00${i}"
     # APP_TEST_POST="300${i}"
-    DIRECTORY=$0
+    DIRECTORY=${0}
     NAME="app-00${i}"
     POST="300${i}"
     echo "Create app test ${DIRECTORY} ${NAME} ${POST}"
