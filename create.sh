7978df16da815748c3298852384af76861cedeee
Blas Medina <r.blas.m.c@gmail.com>
2019-09-11 20:34:44 -0300

    test nginx

--- create.sh
+++ create.sh
@@ -9,7 +9,7 @@ if [ "$1" == "" ]; then
     exit 0
 fi
 
-DIRECTORY=$1
+TARGET_DIRECTORY=$1
 
 ##### Main
 
@@ -18,8 +18,8 @@ DIRECTORY=$1
 # source libs/install-nodejs.sh
 # source libs/install-pm2.sh
 pm2 delete all
-sudo rm -rf $DIRECTORY
-source $PWD/libs/create-apps-test.sh $DIRECTORY 2
+sudo rm -rf $TARGET_DIRECTORY
+source $PWD/libs/create-apps-test.sh $TARGET_DIRECTORY 2
 # source $PWD/libs/install-nginx.sh
-source $PWD/libs/pm2-reload-app.sh $DIRECTORY
+source $PWD/libs/pm2-reload-app.sh $TARGET_DIRECTORY
 exit 0
--- libs/config-nginx-apps.sh
+++ libs/config-nginx-apps.sh
@@ -18,20 +18,13 @@ PATH_FILE=$DIRECTORY/test.txt
 # done
 echo "Create config nginx ${PATH_FILE}"
 sudo sh -c "cat >> ${PATH_FILE}" <<-EOF
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-    server_name _;
-    location /${APP_TEST_NAME}/ {
-        proxy_pass http://localhost:${APP_TEST_POST};
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
-    }
+location /${APP_TEST_NAME}/ {
+    proxy_pass http://localhost:${APP_TEST_POST};
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection 'upgrade';
+    proxy_set_header Host \$host;
+    proxy_cache_bypass \$http_upgrade;
 }
 EOF
 
