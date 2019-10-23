082902ee78c1ad85d16a3dc300bf75aca21c24a5
Blas Medina <r.blas.m.c@gmail.com>
2019-10-23 14:04:50 -0300

    respaldo

--- bms-server.sh
+++ bms-server.sh
@@ -6,8 +6,35 @@ get_public_ip() {
 }
 
 main() {
+    local DOMAIN="blasmedina.cl"
+    create_zone
+}
+
+create_zone() {
+    local DATE=$(date '+%Y%m%d')
     local IP=$(get_public_ip)
-    echo "$IP"
+    local SERIAL="${DATE}00"
+    local HOUR=$((60 * 60))
+    local DAY=$(($HOUR * 24))
+    local WEEK=$(($DAY * 7))
+    local CONTENT=$(cat <<-EOF
+\$ttl $HOUR
+${DOMAIN}.  IN      SOA     $HOSTNAME. root.${DOMAIN}. (
+                        ${SERIAL} ; serial
+                        $(($HOUR * 3)) ; time to refresh
+                        $HOUR ; time to retry
+                        $WEEK ; time to expire
+                        $DAY ; minimum TTL
+            )
+@                                   IN      NS      ${DOMAIN}.
+${DOMAIN}.                      IN      A       ${IP}
+ns1.${DOMAIN}.                  IN      A       ${IP}
+www                                 IN      CNAME   ${DOMAIN}.
+_acme-challenge.${DOMAIN}.  1   IN      TXT     "cfl9U4aJbzfatSG9jyL_JQl6lV6hRtXzGcUPXqsVcOY"
+_acme-challenge.${DOMAIN}.  1   IN      TXT     "WF4_xGNJ5Li6TSlzfHdiKcaOTa9g_A8GNBxTa4Wfhx0"
+EOF
+)
+    echo "$CONTENT"
 }
 
 main "$@"
